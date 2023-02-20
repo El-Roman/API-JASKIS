60eaeebb593cd3117b6ad9240b361d7105a68bcc
Roman M <rmarchen95@gmail.com>
2023-02-19 22:15:48 -0800

    finished DB assignment
    Completed all exercises for db commands

--- commands.js
+++ commands.js
@@ -106,9 +106,21 @@ db.bounties.find({}, { client: 0 })
 db.bounties.find({ species: 'Groundhog', location: 'Woodlands' })
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-
+db.bounties.updateOne({
+    name: 'Polarwind'
+}, { $set: { reward: 10000 } })
 // 2. Remove Lokinkajou
-
+db.bounties.deleteOne({
+    name: 'Lokinkajou'
+}
 // 3. Delete all bounties sent by Songbird
-
+db.bounties.deleteMany({
+    client: 'Songbird'
+})
 // 4. Update all captured statuses to true
+
+db.bounties.updateMany({
+    captured: false
+}, {
+    $set: { captured: true }
+})
